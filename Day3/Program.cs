af70cc12c5a8e0a6b51299ee1e934894299fcf4a
hyperlynx <hjake123@gmail.com>
2025-12-03 12:00:53 -0600

    Solve Day 3

---
+++ Day3/Program.cs
@@ -0,0 +1,71 @@
+﻿Console.WriteLine($"A: {A("./input/input.txt")}");
+Console.WriteLine($"B: {B("./input/input.txt")}");
+
+
+static string A(string in_file)
+{
+    var file_handle = File.OpenText(in_file);
+    if (file_handle == null)
+    {
+        throw new FileNotFoundException($"{in_file} does not exist or is inaccessible");
+    }
+
+    long joltage = 0;
+
+    while (!file_handle.EndOfStream)
+    {
+        string? line = file_handle.ReadLine();
+        if(line == null)
+        {
+            throw new InvalidDataException($"No line before end of stream??");
+        }
+        joltage += NCellJoltage(line, 2);
+    }
+    return $"{joltage}";
+}
+
+static string B(string in_file)
+{
+    var file_handle = File.OpenText(in_file);
+    if (file_handle == null)
+    {
+        throw new FileNotFoundException($"{in_file} does not exist or is inaccessible");
+    }
+
+    long joltage = 0;
+
+    while (!file_handle.EndOfStream)
+    {
+        string? line = file_handle.ReadLine();
+        if (line == null)
+        {
+            throw new InvalidDataException($"No line before end of stream??");
+        }
+        joltage += NCellJoltage(line, 12);
+    }
+    return $"{joltage}";
+}
+
+static long NCellJoltage(string bank, int cell_count)
+{
+    long bank_joltage = 0;
+    int last_digit_index = 0;
+    for (int n = 0; n < cell_count; n++)
+    {
+        bank_joltage *= 10;
+        int nth_digit = 0;
+        int nth_digit_index = 0;
+        for (int i = last_digit_index; i < bank.Length - (cell_count - n - 1); i++) // Reserve enough characters from scanning for future digits to take them
+        {
+            int digit = int.Parse($"{bank[i]}");
+            if (digit > nth_digit)
+            {
+                nth_digit = digit;
+                nth_digit_index = i;
+            }
+        }
+        bank_joltage += nth_digit;
+        last_digit_index = nth_digit_index + 1;
+    }
+    return bank_joltage;
+}
